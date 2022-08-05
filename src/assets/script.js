23d97378982e577e9a5afe847710bc93652f0de8
areeb <mohammedareebali740@gmail.com>
2022-08-05 20:39:10 +0100

    final project submission

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,24 +1,24 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-const products = [{
+let products = [{
 name:'strawberry',
 price:5,
-quantity:3,
-productId:'strawberry',
+quantity:0,
+productId:101,
 image: 'images/strawberry.jpg'
 },
 {
   name:'orange',
   price:6,
-  quantity:3,
-  productId:'orange',
+  quantity:0,
+  productId:102,
   image: 'images/orange.jpg'
   },
   {
     name:'cherry',
     price:10,
-    quantity:4,
-    productId:'cherry',
-    image: 'images/cherry.jpg'
+    quantity:0,
+    productId:103,
+    image: 'images/cherry.jpg',
     }];
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -36,6 +36,16 @@ image: 'images/strawberry.jpg'
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+//helper function prod
+function prod(productId){
+ return products.find (value => value.productId === productId);
+}
+function bag(productId){
+  return cart.find (value => value.productId === productId);
+}
+
+
+let cart = [];
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
@@ -43,34 +53,86 @@ image: 'images/strawberry.jpg'
   - if the product is not already in the cart, add it to the cart
 */
 
+
+function addProductToCart (productId) {
+  
+  const fruit = prod(productId);
+  fruit.quantity += 1
+  if (cart.some (value => value.productId === productId))
+  {return} else {
+    cart.push (fruit);
+  }
+}
+
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
+function increaseQuantity(productId){
+let product = bag(productId);
+console.log(product);
+product.quantity++;
+}
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
-
+function rm(productId){
+  cart.forEach((val , index) => {
+    if(bag(productId) === val && bag(productId).quantity === 0){
+      cart.splice(index , 1);
+    }
+  })
+}
+function decreaseQuantity(productId){
+  let product = bag(productId);
+  product.quantity--;
+rm(productId);
+  }
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
-
+function removeProductFromCart(productId){
+  let product = bag(productId);
+  product.quantity = 0;
+  rm(productId);
+    }
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
 
-/* Create a function called emptyCart that empties the products from the cart */
+function cartTotal() {
+  let total = 0;
+  cart.map(val =>{
+    total+= val.price * val.quantity ;
+  })
+  return total;
+}
 
+/* Create a function called emptyCart that empties the products from the cart */
+function emptyCart(){
+return cart.length = 0;
+
+}
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+let totalPaid = 0;
+let remaining = 0;
+function pay(amount) {
+    totalPaid += amount;
+    remaining = totalPaid - cartTotal();
+    if (totalPaid >= cartTotal())
+        totalPaid = 0;
+
+    return remaining;
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
